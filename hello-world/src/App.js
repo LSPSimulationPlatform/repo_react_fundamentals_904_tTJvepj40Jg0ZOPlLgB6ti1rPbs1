bc264f71f17486606b4cd7c20b6ce9655f3d5c72
Zarangul Babazade <70537610+Zarangul@users.noreply.github.com>
2025-12-28 19:44:09 +0000

    my first project

---
+++ hello-world/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <label style={{fontSize:'24px', color:'darkgoldenrod'}}>It's a beautiful day!</label>
+    </div>
+  );
+}
+
+export default App;
